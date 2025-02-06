4729a27ac4e2f8c257b9f8e50c1bc32216b2b1bf
felippeximenes <felippelpximenes@outlook.com>
2025-02-06 16:57:53 -0300

    key weather adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const inputRef = useRef()
+
+  function searchCity() {
+    const city = inputRef.current.value
+    const key = "ab69bdd46919275bbce3b4a445ad96f6"
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+  }
 
   return (
     <div>
       <h1>Previsão do tempo</h1>
-      <input type="text" placeholder='Digite o nome da cidade' />
-      <button>Buscar</button>
+      <input ref={inputRef} type="text" placeholder='Digite o nome da cidade' />
+      <button onClick={searchCity}>Buscar</button>
       
     </div>
   )
