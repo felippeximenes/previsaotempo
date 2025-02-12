83de0fac8796f39eb35440716b0738fc546d428b
felippeximenes <felippelpximenes@outlook.com>
2025-02-12 10:57:13 -0300

    css aplicado em weatherinformations

--- src/components/WeatherInformations/WeatherInformations.jsx
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -1,17 +1,19 @@
+import './WeatherInformations.css'
+
 function WeatherInformations({ weather }) {
     if (!weather) {
         return <p>Carregando informações...</p>;
     }
 
     return (
-        <div>
+        <div className='weather-container'>
             <h2>{weather.name}</h2>
-            <div>
+            <div className='weather-info'>
                 <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="Weather Icon" />
-                <p>{Math.round(weather.main.temp)} ºC</p>
+                <p className='temperature' >{Math.round(weather.main.temp)} ºC</p>
             </div>
-            <p>{weather.weather[0].description}</p>
-            <div>
+            <p className='description' >{weather.weather[0].description}</p>
+            <div className='details' >
                 <p>Sensação térmica: {Math.round(weather.main.feels_like)} ºC</p>
                 <p>Umidade: {weather.main.humidity} %</p>
                 <p>Pressão: {weather.main.pressure}</p>
